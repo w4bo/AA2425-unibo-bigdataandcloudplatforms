596a08523eb209714bccfb276e6e53243fb86e85
Matteo Francia <m.francia@unibo.it>
2024-10-11 17:45:20 +0200

    refactoring the notebooks

--- slides/generate.sh
+++ slides/generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 # "lab-01-dataunderstanding.ipynb" "lab-02-housing.ipynb" "lab-03-datapreparation.ipynb" "lab-04-housing.ipynb" "lab-05-ml.ipynb" "lab-06-housing.ipynb"
-for FILE in *.ipynb; do 
+for FILE in *lab-01-CaliforniaHousingPricing_v0_0_1.ipynb; do  # *.ipynb
     echo "Processing $FILE file...";
     if [[ "$FILE" == *"lab-00"* ]]; then
         echo "Skipping $FILE"
